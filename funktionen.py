e337238389518bf43124ba1e360429a3a81618cd
Ballerheinz <cle.dinkel@gmail.com>
2020-06-12 19:44:12 +0200

    Neu organisiert und ohne Dicts

--- funktionen.py
+++ funktionen.py
@@ -1,11 +1,11 @@
-def random_race():
+def random_id():
     import random
-    cr_race = random.randint(0, 9)
-    if cr_race >= 7:
-        rasse = "Replikant"
+    cr_id = random.randint(0, 9)
+    if cr_id >= 7:
+        identity = "Replikant"
     else:
-        rasse = "Mensch"
-    return rasse
+        identity = "Mensch"
+    return identity
 
 
 def add_player(name):
@@ -14,45 +14,8 @@ def add_player(name):
 
     random_number = random.randint(1, 5)
     player_loc = locations[random_number]
-    race = random_race()
-    print(f"{name} ist ein {race} und wohnt in {player_loc}")
-
-
-def create_players(player_count):
-    import random
-    from listen import locations
-
-    player_loc_list = list(locations)
-    del player_loc_list[0]
-    del player_loc_list[5:]
-    tuple(player_loc_list)
-    player_loc_list_editable = list(player_loc_list)
-    player_loc_return_list = []
-    player_name_return_list = []
-    spielernummer = 1
-
-    while player_count > 0:
-        if len(player_loc_list) != 0:
-            player_name = input(f"Spieler {spielernummer}: ")
-            player_name_return_list.append(player_name)
-            available_locs = len(player_loc_list_editable)
-            random_number = random.randint(0, available_locs - 1)
-            pc_loc = player_loc_list_editable[random_number]
-            player_loc_return_list.append(pc_loc)
-            player_loc_list_editable.pop(random_number)
-            player_count -= 1
-            spielernummer += 1
-        else:
-            player_loc_list_editable = list(player_loc_list)
-
-    return (player_name_return_list, player_loc_return_list)
-
-
-def random_npc_loc(first_loc, last_loc):
-    import random
-    from listen import locations
-    npc_loc = locations[random.randint(first_loc, last_loc)]
-    return npc_loc
+    identity = random_id()
+    print(f"{name} ist ein {identity} und wohnt in {player_loc}")
 
 
 def random_npc_cityloc_even(iterations):
